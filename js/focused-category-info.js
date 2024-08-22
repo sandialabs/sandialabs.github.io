6d8c6d15a40bdbf7c44c575b382246691beb420d
mrmundt <mrmundt@users.noreply.github.com>
2024-08-22 06:56:11 +0000

    2024-08-22-06 Data Update

--- js/focused-category-info.js
+++ js/focused-category-info.js
@@ -68,7 +68,7 @@ angular.module('app', []).controller('gitHubDataController', [
                     path: '/assets/images/all_icon_sandia.svg',
                     alt: 'All Software'
                 },
-                description: 'Browse all 741 Sandia open source projects',
+                description: 'Browse all 742 Sandia open source projects',
                 displayTitle: 'All Software',
                 topics: []
             };
